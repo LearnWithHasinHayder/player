8fa2243467e9cb27adcba885b0a0130be65adec5
Hasin Hayder <hasin.hayder.official@gmail.com>
2024-02-26 22:01:18 +0600

    Player

---
+++ src/components/Navigation.vue
@@ -0,0 +1,21 @@
+<script setup>
+import course from "../data/data"
+</script>
+<template>
+  <div class="navigation bg-gray-900 h-[var(--nav-height)] flex justify-between items-center sticky top-0 z-10">
+    <div class="px-2 text-xl text-white w-[var(--sidebar-width)] flex justify-between">
+      <span class="text-lg text-gray-400">{{ course.title }}</span>
+    </div>
+    <div class="pr-5 text-base text-white w-[calc(100%-var(--sidebar-width))] flex justify justify-between items-center">
+      <div class="sm:invisible lg:visible">
+        <p class="text-base pl-5 text-gray-300">Completed 16%</p>
+      </div>
+      <ul class="flex justify-end space-x-6">
+        <li>Settings</li>
+        <li>Home</li>
+        <li>Account</li>
+        <li>Logout</li>
+      </ul>
+    </div>
+  </div>
+</template>
---
+++ src/data/engine.js
@@ -0,0 +1,61 @@
+import { reactive } from 'vue'
+import axios from 'axios'
+import course from './data'
+
+const engine = reactive({
+  initialize () {
+    console.log('Engine initialized')
+    axios
+      .get(
+        'https://62dd8310ccdf9f7ec2c91b1b.mockapi.io/api/masterin-alpine-js-demo'
+      )
+      .then(response => {
+        course.chapters = response.data.chapters
+        course.intro = response.data.intro
+        course.title = response.data.title
+        course.id = response.data.id
+        this.player.init()
+      })
+    this.player.currentEpisode = course.intro
+  },
+  player: {
+    currentEpisode: {},
+    episodes: [],
+    init () {
+      course.chapters.forEach(c => {
+        c.episodes.forEach(e => {
+          this.episodes.push(e)
+        })
+      })
+    },
+    play (episode) {
+      this.currentEpisode = episode
+    },
+    playNext () {
+      let nextIndex = 0
+      const currentEpisodeUuid = this.currentEpisode.uuid
+      if (currentEpisodeUuid) {
+        nextIndex =
+          this.episodes.findIndex(e => e.uuid == currentEpisodeUuid) + 1
+      }
+      if (this.episodes[nextIndex]) {
+        const nextEpisode = this.episodes[nextIndex]
+        this.play(nextEpisode)
+      }
+    },
+    playPrevious () {
+      let prevIndex = 0
+      const currentEpisodeUuid = this.currentEpisode.uuid
+      if (currentEpisodeUuid) {
+        prevIndex =
+          this.episodes.findIndex(e => e.uuid == currentEpisodeUuid) - 1
+      }
+      if (this.episodes[prevIndex]) {
+        const prevEpisode = this.episodes[prevIndex]
+        this.play(prevEpisode)
+      }
+    }
+  }
+})
+
+export default engine
